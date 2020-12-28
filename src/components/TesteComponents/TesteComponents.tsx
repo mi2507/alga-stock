d1c6e2e736f2d762a3ec17744d5d44ed8672ba2b
mi2507 <mreginadasilva@yahoo.com.br>
2020-12-28 17:04:17 -0300

    create testcomponent and move to the components directory

---
+++ src/components/TesteComponents/TesteComponents.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './TesteComponents.css'
+
+const TestComponents = () => (
+  <div className="TestComponents" > Test Components 
+  <p>feito por Michelle Regina</p>
+  </div>)
+
+export default TestComponents
