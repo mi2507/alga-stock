1cf03edcbc301a739800c7dc8a0f7fc0e100c705
mi2507 <mreginadasilva@yahoo.com.br>
2020-12-29 10:40:20 -0300

    entendendo o hook-useeffect

--- src/components/TesteComponents/TesteComponents.tsx
+++ src/components/TesteComponents/TesteComponents.tsx
@@ -1,14 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './TesteComponents.css'
 
 function TestComponents(props: { name: string }) {
   const [age, setAge] = useState(34)
+
+  useEffect(() => {
+    console.log ('component was created')
+  }, [])
+
+  useEffect(() => {
+    console.log ('Age has been update to:' + age)
+  }, [age])
   
+  
+
   return <div className="TestComponents" > Test Components,
      olá {props.name}, {age}
     <button onClick={() => {
       setAge (age +1)
     }}> +
+    </button>
+    <button onClick={() => {
+      setAge (age -1)
+    }}> -
     </button>
   </div>
 }
