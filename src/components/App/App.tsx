56604be384591454c421bd94b253e57d19e70ae0
mi2507 <mreginadasilva@yahoo.com.br>
2020-12-30 11:50:40 -0300

    arquivos alterados

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestComponents from '../TesteComponents';
-import ClassComponents from '../ClassComponents';
+import Header from './Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        < TestComponents name = "Michelle"/>
-        < ClassComponents name="Mundo !!!"/>
-      </header>
+    
+      <Header title="AlgaStock" />
+      App
     </div>  
   );
 }
