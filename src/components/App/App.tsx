9331a6b37577bbd684477c48809b39bb19c2bc07
mi2507 <mreginadasilva@yahoo.com.br>
2020-12-30 17:51:33 -0300

    adicionado componente Input

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header/';
 import Button from '../../shared/Button'
 import Container from '../../shared/Container/';
+import Input from '../../shared/Input/';
+
 
 
 function App() {
+  const [street, setStreet] = useState('')
   return (
     <div className="App">
     
@@ -13,8 +16,17 @@ function App() {
 
       <Container>
 
-        <Button content="Botão" onClick = { () => window.alert ("isto é um teste!")} />
-        </Container>
+        <Button content="Botão" onClick={() => window.alert("isto é um teste!")} />
+
+        <Input label="street"
+          placeholder="E.g.: 15h Avenue"
+          value={street}
+          onChange= {e => setStreet (e.target.value)}
+        />
+
+      </Container>
+      
+
     
     </div>  
   );
