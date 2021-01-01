e302f16aab2245d84cf22f126456c5a837a53ca7
mi2507 <mreginadasilva@yahoo.com.br>
2021-01-01 17:21:38 -0300

    como fazer inclusao de produto no form e o mesmo aparecer na tabela

--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -1,58 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Form from '../../shared/Form'
-import Input from '../../shared/Input'
-import Button from '../../shared/Button'
+import Form from "../../shared/Form";
+import Input from "../../shared/Input";
+import Button from "../../shared/Button";
 
 const initialFormState = {
-  name: '',
-  price: '',
-  stock: ''
+  name: "",
+  price: "",
+  stock: "",
+};
+
+export interface ProductCreator {
+  name: string
+  price: number
+  stock: number
 }
 
-const ProductForm = () => {
-  const [form, setForm] = useState(initialFormState)
+declare interface ProductFormProps{
+onSubmit: (product: ProductCreator) => void
+}
+
+
+const ProductForm: React.FC <ProductFormProps> = (props) => {
+  const [form, setForm] = useState(initialFormState);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = event.target
+    const { value, name } = event.target;
 
     setForm({
       ...form,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
-  return <Form title="Product Form" onSubmit={() => console.log(form)}>
-    <Input
-      onChange={handleInputChange}
-      name="name"
-      label="Name"
-      placeholder="E.g.: Cookie"
-      required
-    />
-    <Input
-      onChange={handleInputChange}
-      name="price"
-      label="Price"
-      type="number"
-      step="0.01"
-      min="0"
-      placeholder="E.g.: 1.25"
-      required
-    />
-    <Input
-      onChange={handleInputChange}
-      name="stock"
-      label="Stock"
-      type="number"
-      min="0"
-      placeholder="E.g.: 15"
-      required
-    />
-    <Button>
-      Submit
-    </Button>
-  </Form>
-}
+  const hendleFormSubmit = () => {
+    const productDto = {
+      name: String (form.name),
+      price: parseFloat (form.price),
+      stock: Number (form.stock)
+    }
+    props.onSubmit(productDto)
+    setForm(initialFormState)
+ }
 
-export default ProductForm
+
+  return (
+    <Form title="Product Form" onSubmit={hendleFormSubmit}>
+      <Input
+        onChange={handleInputChange}
+        value={form.name}
+        name="name"
+        label="Name"
+        placeholder="E.g.: Cookie"
+        required
+      />
+      <Input
+        onChange={handleInputChange}
+        value={form.price}
+        name="price"
+        label="Price"
+        type="number"
+        step="0.01"
+        min="0"
+        placeholder="E.g.: 1.25"
+        required
+      />
+      <Input
+        onChange={handleInputChange}
+        value={form.stock}
+        name="stock"
+        label="Stock"
+        type="number"
+        min="0"
+        placeholder="E.g.: 15"
+        required
+      />
+      <Button>Submit</Button>
+    </Form>
+  );
+};
+
+export default ProductForm;
