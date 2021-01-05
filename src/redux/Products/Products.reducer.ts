c010f23c34441e215d18a2ab58c12a02f92cc5d5
mi2507 <mreginadasilva@yahoo.com.br>
2021-01-05 13:51:25 -0300

    disparando actions com componentes

--- src/redux/Products/Products.reducer.ts
+++ src/redux/Products/Products.reducer.ts
@@ -1,12 +1,18 @@
+
+import Products, { Product } from "../../shared/Table/Table.mockdata"
+
 export interface Action<T = any> {
   type: string
   payload?: T
 }
 
-export default function (state = [], action: Action) {
+export default function (state = Products, action: Action): Product[] {
   switch (action.type) {
     case 'INSERT_NEW_PRODUCT':
-      return [...state, action.payload]
+      return [...state, {
+        ...action.payload,
+        _id: String(state.length + 1)
+      }]
     default:
       return state
   }
