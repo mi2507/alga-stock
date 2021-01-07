b9342f5a3b7fe38faf97b7f714a94fb58d72fc20
mi2507 <mreginadasilva@yahoo.com.br>
2021-01-07 14:05:21 -0300

    corrigindo autenticacao

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -6,19 +6,31 @@ import {
 } from 'redux'
 import thunk, { ThunkAction } from 'redux-thunk'
 import Products from './Products/Products.reducer'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import Authentication from './Authentication/Authentication.reducer'
 
 const reducers = combineReducers({
-  products: Products
+  products: Products,
+  authentication: Authentication
 })
 
+const persistedReducer = persistReducer({
+  key: 'algastock',
+  storage,
+  blacklist: ['products']
+}, reducers)
+
 const store = createStore(
-  reducers,
+  persistedReducer,
   compose(
     applyMiddleware(thunk),
     // @ts-ignore
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
+
+const persistor = persistStore(store)
 
 export interface Action<T = any> {
   type: string
@@ -32,4 +44,4 @@ export type Thunk<T = any> =
 
 export type ThunkDispatch = (thunk: Thunk) => Promise<Thunk>
 
-export default store
+export { store, persistor }
